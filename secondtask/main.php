dbd704a32464f7500986f2e060ce0a382cfb7c35
Acidburn007 <sandesh.bishokarma@student.hamk.fi>
2026-02-08 23:12:46 +0545

    Initial Commit after organizing

---
+++ secondtask/main.php
@@ -0,0 +1,11 @@
+<? include 'header.php';
+?>
+<!--Main Content goes here-->
+<main>
+        <h2>About This Website</h2>
+        <p>This is a simple webpage that you can edit to include your content. It includes a header and footer, with a body area where you can add information for your users.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas expedita eum, nihil soluta sapiente a deleniti dicta sit possimus cumque necessitatibus harum, rem, sint aspernatur? Non sint quos repellendus magnam. Odit inventore nulla cupiditate ratione impedit itaque quaerat pariatur qui vel minima iusto doloribus id aut, fugiat fugit non tempora.</p>
+    </main>
+    
+<? include 'footer.php';
+?>
