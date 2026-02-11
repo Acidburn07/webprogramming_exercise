eddbb40abcdad63994aeeb4b6aa0e62aa7765c5d
Acidburn007 <sandesh.bishokarma@student.hamk.fi>
2026-02-12 04:06:49 +0545

    Modified footer according to ex3

--- ex2/footer.php
+++ ex2/footer.php
@@ -1,7 +1,22 @@
-</main> </div> <footer class="bg-[#333] text-white p-6 text-center mt-auto">
-        <div class="container mx-auto">
-            <p class="text-gray-300">This website is created for educational purposes in web programming.</p>
+</main> 
+</div> 
+
+<footer class="bg-[#333] text-white p-6 text-center mt-auto">
+    <div class="container mx-auto">
+        <p class="text-gray-300">This website is created for educational purposes in web programming.</p>
+        
+        <div class="mt-4 text-xs text-gray-400 italic">
+            <?php
+                // Get the current file name
+                $filename = basename($_SERVER['PHP_SELF']);
+                // Get the last modification time
+                $filetime = filemtime($filename);
+                // Print the formatted date and time
+                echo "Last modified: " . date("F d Y H:i:s.", $filetime);
+            ?>
         </div>
-    </footer>
+    </div>
+</footer>
+
 </body>
 </html>
